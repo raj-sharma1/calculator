c1e9f92cc358473086bedeb549ce4189cd6d3d2d
RAJESH SHARMA <raj.sharma21@outlook.com>
2019-03-06 15:13:24 +1300

    add pseudocode

---
+++ pseudocode.js
@@ -0,0 +1,32 @@
+
+
+
+//create an Html page for the calculator.
+//assign the <div> element for generic flow of content
+//create class and id attribute for calculator, freecodecamp and electronic calculator
+//input element for text
+//create row and assign class element to all the row
+// create button for AC and CE and name a different class but should be the same for both of them
+//create button for %, divide
+//create another row for 7,8,9,x button
+//create another row for 4,5,6,- button
+//create another row for 1,2,3 and a button class tall for +
+//create another row for 0,.,= button
+
+//for styling(css)
+
+//import the url
+//style the body, html with width, background-color
+//style the class calculator with a period sign such as width, height, background-color and margin
+//style the curved edges, left and right shadows and title of the calculator
+//style the id tagline with #
+//number input box, style the input type text
+//style the buttons
+//style the ctrl class with a period sign for AC and CE
+// style the tall class with a period sign for +
+
+
+
+
+
+
