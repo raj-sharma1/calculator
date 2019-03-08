7180f96d1de1902d50d9b50516db7b41d53ddfa3
RAJESH SHARMA <raj.sharma21@outlook.com>
2019-03-08 18:04:06 +1300

    update the file

--- pseudocode.js
+++ pseudocode.js
@@ -2,15 +2,19 @@
 
 
 //create an Html page for the calculator.
-//assign title as calculator
-//inside body create h1 freecodecamp and h2 electronic calculator heading
+//assign title as calculator in head tag.
+//inside the body create h1 freecodecamp and h2 electronic calculator heading
 //assign the <form> tag for generic flow of content, name it calculator and id attributes to "main"
-// create an input element, id attribute and give the name display with a placeholder "0"
+// create an input element equal to type text, id attribute and give the name display, a placeholder "0"
 
-// create a div tag assing id to it with the name "subdiv"
+// create a div tag assign id to it with the name "subdiv"
 
-//create an input type button with an id name, create an onclick event and assign a function to it with a specific value in this case "AC"
-//repeat the above process for CE, %, /,  7,8,9,x, 4,5,6,-, 1,2,3, 0,.,=
+//create an input type button with an id name "numclear", create an onclick event equal to clearDisplay function to it with a specific value in this case "AC"
+//create an input type button with an id name "numback", create an onclick event equal to deleteNumbers function to it with a specific value in this case "CE"
+//create an input type button with an id name "numequal", create an onclick event equal to displayResults function to it with a specific value in this case "="
+
+//create an input type button with an id name for each different number and operator sign, create an onclick event equal to addToScreens function to it with a respective value to each id.
+
 //create a break line after 4 input so that the calculator will have 5 rows.
 
 
@@ -28,13 +32,13 @@
 //style the form with margin and text-align
 
 // for Javascript
-//define a fucntion clearDisplay, assign a variable to find an HTML element and returns the variable with a value equal to empty strings
-/*define a function displayResults, assing a variable which will be equal to the respective value, 
-add another variable and assing a a math (eval)expression which will return and be equal to the result*/
-//define a function deleteNumbers, assing the same variable from displayResults which will be equal to the respective value, 
+//define a function clearDisplay, assign a variable to find an HTML element and returns the variable with a value equal to empty strings
+/*define a function displayResults, assign a variable expression which will be equal to the respective value, 
+add another variable result and assign a a math (eval)expression which will return and be equal to the result*/
+//define a function deleteNumbers, assign the same variable from displayResults which will be equal to the respective value, 
 //add another variable which will return the subtraction from each expression length by minus 1 */
-//Define a function addToScreen with a parenthesis x, 
-//assign a variable to find an HTML element and returns adding to the variable */
+//Define a function addToScreen with a parameter x in the parenthesis, 
+//assign a variable to find an HTML element and returns adding to the variable each time its call */
 
 
 
