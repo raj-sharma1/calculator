c3de24f25e264b1646b52189b09bd3e95f2b2028
RAJESH SHARMA <raj.sharma21@outlook.com>
2019-03-08 15:14:22 +1300

    update pseudo

--- pseudocode.js
+++ pseudocode.js
@@ -2,29 +2,39 @@
 
 
 //create an Html page for the calculator.
-//assign the <div> element for generic flow of content
-//create class and id attribute for calculator, freecodecamp and electronic calculator
-//input element for text
-//create row and assign class element to all the row
-// create button for AC and CE and name a different class but should be the same for both of them
-//create button for %, divide
-//create another row for 7,8,9,x button
-//create another row for 4,5,6,- button
-//create another row for 1,2,3 and a button class tall for +
-//create another row for 0,.,= button
+//assign title as calculator
+//inside body create h1 freecodecamp and h2 electronic calculator heading
+//assign the <form> tag for generic flow of content, name it calculator and id attributes to "main"
+// create an input element, id attribute and give the name display with a placeholder "0"
+
+// create a div tag assing id to it with the name "subdiv"
+
+//create an input type button with an id name, create an onclick event and assign a function to it with a specific value in this case "AC"
+//repeat the above process for CE, %, /,  7,8,9,x, 4,5,6,-, 1,2,3, 0,.,=
+//create a break line after 4 input so that the calculator will have 5 rows.
+
+
 
 //for styling(css)
+//style the main section of form by border, width, height,border-raidus and text align
+//style the h1 with text-align
+//style the h2 with text-align and font-size
+//style the input display, width, margin, height, border and font-size
+//style the button input from num7 to numdec with background-color, cursor,width, height, padding, box-shadow,margin 
+//style the button input numback for different background-color cursor,width, height, padding, box-shadow,margin 
+//style the button input numclear for different background-color cursor,width, height, padding, box-shadow,margin 
+//style the button input numequal for different background-color cursor,width, height, padding, box-shadow,margin 
+//style the div-subdiv with margin left and right
+//style the form with margin and text-align
 
-//import the url
-//style the body, html with width, background-color
-//style the class calculator with a period sign such as width, height, background-color and margin
-//style the curved edges, left and right shadows and title of the calculator
-//style the id tagline with #
-//number input box, style the input type text
-//style the buttons
-//style the ctrl class with a period sign for AC and CE
-// style the tall class with a period sign for +
-
+// for Javascript
+//define a fucntion clearDisplay, assign a variable to find an HTML element and returns the variable with a value equal to empty strings
+/*define a function displayResults, assing a variable which will be equal to the respective value, 
+add another variable and assing a a math (eval)expression which will return and be equal to the result*/
+//define a function deleteNumbers, assing the same variable from displayResults which will be equal to the respective value, 
+//add another variable which will return the subtraction from each expression length by minus 1 */
+//Define a function addToScreen with a parenthesis x, 
+//assign a variable to find an HTML element and returns adding to the variable */
 
 
 
